c2fdfb6c68b93cd7fd8532e8c648cf34be4ef311
ersharad <sharad@krispcallmail.com>
2024-09-11 20:23:27 +0545

    Salestrack core setting factory

--- salestrack/core/config.py
+++ salestrack/core/config.py
@@ -1,51 +1,55 @@
-from typing import Any, Dict, List, Optional, Union
-from pydantic import AnyHttpUrl, PostgresDsn, field_validator, ValidationInfo
-from pydantic_settings import BaseSettings, SettingsConfigDict
+# from typing import Any, Dict, List, Optional, Union
+# from pydantic import AnyHttpUrl, PostgresDsn, field_validator, ValidationInfo
+# from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
-class Settings(BaseSettings):
-    PROJECT_NAME: str
-    BACKEND_CORS_ORIGINS: Union[List[AnyHttpUrl], List[str]] = []
+# class Settings(BaseSettings):
+#     PROJECT_NAME: str
+#     BACKEND_CORS_ORIGINS: Union[List[AnyHttpUrl], List[str]] = []
     
-    model_config = SettingsConfigDict(
-        env_file=".env", 
-        case_sensitive=True
-    )
+#     model_config = SettingsConfigDict(
+#         env_file=".env", 
+#         case_sensitive=True
+#     )
 
-    @field_validator("BACKEND_CORS_ORIGINS", mode="before")
-    def assemble_cors_originals(cls, v: Union[str, List[str]]) -> Union[List[str], str]:
-        if isinstance(v, str) and not v.startswith("["):
-            return [i.strip() for i in v.split(",")]
-        elif isinstance(v, (list, str)):
-            return v
-        raise ValueError(v)
+#     @field_validator("BACKEND_CORS_ORIGINS", mode="before")
+#     def assemble_cors_originals(cls, v: Union[str, List[str]]) -> Union[List[str], str]:
+#         if isinstance(v, str) and not v.startswith("["):
+#             return [i.strip() for i in v.split(",")]
+#         elif isinstance(v, (list, str)):
+#             return v
+#         raise ValueError(v)
     
-    POSTGRES_SERVER: str
-    POSTGRES_USER: str
-    POSTGRES_PASSWORD: str
-    POSTGRES_DB: str
-    DATABASE_URI: Union[Optional[PostgresDsn], str] = None
+#     POSTGRES_SERVER: str
+#     POSTGRES_USER: str
+#     POSTGRES_PASSWORD: str
+#     POSTGRES_DB: str
+#     DATABASE_URI: Union[Optional[PostgresDsn], str] = None
 
-    @field_validator("DATABASE_URI", mode="before")
-    def assemble_db_connection(cls, v: Optional[str], values: ValidationInfo) -> Any:
-        if isinstance(v, str):
-            return v
-        return PostgresDsn.build(
-            scheme="postgresql",
-            username=values.data.get("POSTGRES_USER"),
-            password=values.data.get("POSTGRES_PASSWORD"),
-            host=values.data.get("POSTGRES_SERVER"),
-            path=f"/{values.data.get('POSTGRES_DB') or ''}",
-        )
+#     @field_validator("DATABASE_URI", mode="before")
+#     def assemble_db_connection(cls, v: Optional[str], values: ValidationInfo) -> Any:
+#         if isinstance(v, str):
+#             return v
+#         return PostgresDsn.build(
+#             scheme="postgresql",
+#             username=values.data.get("POSTGRES_USER"),
+#             password=values.data.get("POSTGRES_PASSWORD"),
+#             host=values.data.get("POSTGRES_SERVER"),
+#             path=f"/{values.data.get('POSTGRES_DB') or ''}",
+#         )
     
-    #JWT
-    ACCESS_TOKEN_EXPIRE_MINUTES: int
-    REFRESH_TOKEN_EXPIRE_MINUTES: int
-    ALGORITHM: str
-    JWT_SECRET_KEY: str
-    JWT_REFRESH_SECRET_KEY: str
+#     #JWT
+#     ACCESS_TOKEN_EXPIRE_MINUTES: int
+#     REFRESH_TOKEN_EXPIRE_MINUTES: int
+#     ALGORITHM: str
+#     JWT_SECRET_KEY: str
+#     JWT_REFRESH_SECRET_KEY: str
     
-    #for testcase
-    TEST_DB_URI: Union[Optional[PostgresDsn], str] = None
+#     #for testcase
+#     TEST_DB_URI: Union[Optional[PostgresDsn], str] = None
 
-settings = Settings()
+# settings = Settings()
+
+
+from pathlib import Path
+from salestrack.core.settings import Settings
--- salestrack_utils/salestrack/core/settings.py
+++ salestrack_utils/salestrack/core/settings.py
@@ -1,8 +1,11 @@
+import os
+import sys
 import typing
 from pathlib import Path
+from yaml import safe_load
 from structlog import get_logger
 from pydantic_settings import BaseSettings
-from pydantic import PostgresDsn
+from pydantic import PostgresDsn, ValidationError
 
 logger = get_logger("__name__")
 
@@ -30,3 +33,35 @@ class CoreSettings(BaseSettings):
     class Config: 
         """env_prefix should be defined by child Settings"""
         env_file = "production.env"
+
+
+def _load_config(
+    app_env: str, config_dir: Path
+) -> typing.Dict[typing.Any, typing.Any]:
+    """load config file"""
+    config_file = config_dir.joinpath(f"{app_env}.yaml")
+    if not config_file.exists():
+        raise ValueError(f"Config file {config_file} doesn't exists")
+    logger.info("Found Configuration ... %s", config_file)
+    return safe_load(open(config_file, "r").read())
+
+
+def settings_factory(
+    settings_class: typing.Type[CoreSettings], env_prefix: str, base_path: Path
+) -> CoreSettings:
+    """generate settings object using provided class"""
+    app_env = os.getenv(env_prefix + "APP_ENV", "production")
+    config_dir = base_path.joinpath("config")
+    try: 
+        settings = settings_class(
+            _env_file=f"{app_env}.env",
+            base_path=base_path,
+            app_env=app_env,
+            **_load_config(app_env, config_dir),
+        )
+    except ValidationError as e:
+        logger.error("Invalid Config/Settings.")
+        logger.exception(e)
+        sys.exit(-1)
+    else:
+        return settings
